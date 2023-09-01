5b203c5a75eda1360b0c0ba35a39ecdef95149b0
Anjali Purohit <143180602+AnjaliPurohit2811@users.noreply.github.com>
2023-09-01 16:23:04 +0530

    Add files via upload

---
+++ biodata.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+       printf("Name = Anjali purohit\n");
+       printf("Age = 21\n");
+       printf("Degree = b.com graduate\n");
+       printf("Date of Birth = 06/10/2001\n");
+       printf("Phone Number = 9601046183\n");
+       printf("E-mail = anjalipurohit2811@gmail.com\n");
+       printf("Father Name = Dashrath bhai\n");
+       printf("Mother Name = Daxa ben\n");
+       printf("College Name = Evening Commerce college\n");
+       
+
+}
