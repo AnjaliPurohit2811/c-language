1e823d6f2fa3d2c0d15b97de1867a8daacb143e2
Anjali Purohit <143180602+AnjaliPurohit2811@users.noreply.github.com>
2023-10-12 16:43:55 +0530

    12/10/23

---
+++ Filament/compare.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<string.h>
+
+main()
+{
+	
+	char a[20];
+	char b[20];
+	
+	puts("enter the string of a=");
+	gets(a);
+	
+	puts("enter the string of b=");
+	gets(b);
+	
+	int c=strcmp(a,b);
+	
+	if(c==strcmp(a,b))
+	{
+		puts("both string are equal");
+	}
+	else
+	{
+		puts("both string are not equal");
+	}
+	
+	
+}
